e634a8d5aaa1e1526ac21437b077d618407b5339
k0sukey <naughtydog0721@gmail.com>
2011-12-30 23:01:43 +0900

    Support refreshAuthentication

--- Resources/evernote/authenticate.js
+++ Resources/evernote/authenticate.js
@@ -9,16 +9,28 @@ namespace('EvCl.Evernote', function(exports){
 			config.consumerKey, config.consumerSecret, 
 			function(event) {
 				if(event.type == 'success') {
-					var auth = {
-						shardId:event.result.user.shardId,
-						token:event.result.authenticationToken
-					};
-					exports.auth = auth;
-					args.success(auth);
+					Ti.App.Properties.setString('email', event.result.user.email);
+					Ti.App.Properties.setString('authenticationToken', event.result.authenticationToken);
+					Ti.App.Properties.setString('userShardId', event.result.user.shardId);
+					Ti.App.Properties.setDouble('expired', new Date().getTime() + (event.result.expiration - event.result.currentTime));
+					args.success();
 				} else {
 					args.error(event.error);
 				}
 			}
 		);
+	};
+	
+	exports.refreshAuthentication = function(args){
+		var userstore = api.createUserStoreClient(config.url+'user');
+		userstore.refreshAuthentication(Ti.App.Properties.getString('authenticationToken'), function(event){
+			if (event.type == 'success') {
+				Ti.App.Properties.setString('authenticationToken', event.result.authenticationToken);
+				Ti.App.Properties.setDouble('expired', new Date().getTime() + (event.result.expiration - event.result.currentTime));
+				args.success();
+			} else {
+				args.error(event.error);
+			}
+		});
 	};
 });
